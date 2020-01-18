ee4c96d4328db45dd95e5a2222272fb669a0e245
Phil Isaacs <32342350+LemonCow@users.noreply.github.com>
2020-01-18 13:39:56 -0500

    shooterMotorSubsystem with it correctly setup

--- src/main/java/frc/robot/subsystems/shooterMotorsSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooterMotorsSubsystem.java
@@ -9,12 +9,14 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+shooterMotorsSubsystem extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
    */
-  public ExampleSubsystem() {
+  public shooterMotorSubsystem() {
 
+    
+  
   }
 
   @Override
