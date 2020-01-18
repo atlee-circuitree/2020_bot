d39d4f4a0186a8ed3ca03f42882871517cc49433
Circuitree Laptop New <dontknowouremail@gmail.com>
2020-01-18 13:39:07 -0500

    Updates Robot.java to the working drivetrain

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,107 +7,222 @@
 
 package frc.robot;
 
+//import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.PWMSpeedController;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.Encoder;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+//Limelight Imports
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj.VictorSP;
+
+
+@SuppressWarnings("deprecation")
+
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+    DifferentialDrive robotDrive;
+    // Talon frontLeft, frontRight, rearLeft, rearRight;
+    Spark frontLeft, frontRight, rearLeft, rearRight;
 
-  private RobotContainer m_robotContainer;
+    XboxController stick;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
-  }
+    // public static final double kDistancePerRevolution = 18.84; // guestimate from
+    // your code
+    // public static final double kPulsesPerRevolution = 4096; // for an talon?
+    // maybe
+    // public static final double kDistancePerPulse = kDistancePerRevolution /
+    // kPulsesPerRevolution;
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
-
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   */
-  @Override
-  public void disabledInit() {
-  }
-
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /**
-   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+    /**
+     * Constructor. Called once when this class is created.
+     */
+    public Robot() {
+        System.out.println("Robot.constructor()");
     }
-  }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-  }
+    /**
+     * Called once after class load complete. Use to perform any needed
+     * initializations. Very similar to the constructor.
+     */
 
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    public void robotInit() {
+
+        
+        System.out.println("Robot.robotInit()");
+        // Create two PWM Talon motor controller objects for left & right motors on PWM
+        // ports 0 & 1.
+        // Assumes robot has two motors controlled by Talon controllers connected via
+        // PWM.
+        // Add them to a drive controller class that can do tank and arcade driving
+        // based on
+        // joystick input.
+
+        // public class Robot{}
+
+        frontLeft = new Spark(8);
+        rearLeft = new Spark(9);
+        // frontLeft = new Talon(1);
+        // rearLeft = new Talon(0);
+        SpeedControllerGroup leftDrive = new SpeedControllerGroup(frontLeft, rearLeft);
+
+        frontRight = new Spark(7);
+        rearRight = new Spark(6);
+        // frontRight = new Talon(3);
+        // rearRight = new Talon(2);
+        SpeedControllerGroup rightDrive = new SpeedControllerGroup(frontRight, rearRight);
+
+        robotDrive = new DifferentialDrive(leftDrive, rightDrive);
+
+        robotDrive.setExpiration(0.2); // need to see motor input at least every
+                                       // 10th of a second or stop motors.
+
+        // One side has motors reversed so the wheels turn in the same direction.
+        robotDrive.setRightSideInverted(true);
+
+        stick = new XboxController(0); // joystick on usb port 0.
+
     }
-  }
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-  }
+    /**
+     * Executes a simple autonomous program. Called by the driver station or field
+     * control system at the start of the autonomous period.
+     */
+    public void autonomous() {
+        System.out.println("Robot.autonomous()");
 
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
+        robotDrive.setSafetyEnabled(false); // motor safety off due to the fact
+                                            // we want the motor to run 2 sec
+                                            // with no other input.
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+        robotDrive.tankDrive(0.5, 0.5); // drive forwards half speed
+        Timer.delay(2.0); // for 2 seconds.
+        robotDrive.tankDrive(0.0, 0.0); // stop motors.
+    }
+   /**
+    * Runs the motors with arcade steering, input from joystick.
+    * Called by the driver station or field control system at the
+    * start of the operator control (teleop) period. Runs in a loop
+    * until robot is disabled.
+    */
+   public void operatorControl()
+   {
+       System.out.println("Robot.operatorControl()");
+ 
+       robotDrive.setSafetyEnabled(true);   // motor safety back on.
+
+        
+  
+       while (isOperatorControl() && isEnabled())
+       {
+           robotDrive.arcadeDrive(-stick.getY()/2, stick.getX()/2);   // drive with arcade style.
+           robotDrive.tankDrive(-stick.getY(Hand.kLeft)/2, -stick.getY(Hand.kRight)/2);
+           Timer.delay(2.0);
+           
+           // wait for a joystick update.
+
+       }
+    }
+          private boolean m_LimelightHasValidTarget = false;
+          private double m_LimelightDriveCommand = 0.0;
+          private double m_LimelightSteerCommand = 0.0;
+    
+    public void Update_Limelight_Tracking()
+    {
+          // These numbers must be tuned for your Robot!  Be careful!
+          final double STEER_K = 0.03;                    // how hard to turn toward the target
+          final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
+          final double DESIRED_TARGET_AREA = 13.0;        // Area of the target when the robot reaches the wall
+          final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
+  
+          double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+          double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+          double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+          double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+  
+          if (tv < 1.0)
+          {
+            m_LimelightHasValidTarget = false;
+            m_LimelightDriveCommand = 0.0;
+            m_LimelightSteerCommand = 0.0;
+            return;
+          }
+  
+          m_LimelightHasValidTarget = true;
+  
+          // Start with proportional steering
+          double steer_cmd = tx * STEER_K;
+          m_LimelightSteerCommand = steer_cmd;
+  
+          // try to drive forward until the target area reaches our desired area
+          double drive_cmd = (DESIRED_TARGET_AREA - ta) * DRIVE_K;
+  
+          // don't let the robot drive too fast into the goal
+          if (drive_cmd > MAX_DRIVE)
+          {
+            drive_cmd = MAX_DRIVE;
+          }
+          m_LimelightDriveCommand = drive_cmd;
+    }
+    
+    /**
+     * Called whenever the robot is disabled by the DS or field control. Use to
+     * perform any needed resets or changes when switching between modes.
+     */
+    public void disabled() {
+        System.out.println("Robot.disabled()");
+    }
+
+    /**
+     * Runs during test mode
+     */
+    public void test() {
+        System.out.println("Robot.test()");
+
+        // Limelime
+
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").getDouble(0);
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").setNumber(1);
+
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
+
+        // read values periodically
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
+
+        // post to smart dashboard periodically
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
+
+    }
 }
